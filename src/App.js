0ed52256cb766185605e92a69e2e55c8daaa3c6e
Saket D Shetty <shettysaket05@gmail.com>
2020-06-01 19:48:27 +0530

    content fix

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ class App extends React.Component {
     return (
       <div>
         <p>Hi! My name is Saket Shetty.</p>
-        <p>I am currently pursuing Engineering in Information Technology from Pillai College of Engineering (2016-2020).</p>
-        <p>My final year project is building a <a href="https://github.com/saket-shetty/Human-Posture-Recognition">Human Posture Detection</a> program that will detect human posture from images and video using ML.</p>
-        <p>I like to work on mobile app technology like Flutter and Android(Java).</p>
+        <p>I have completed Engineering in Information Technology from Pillai College of Engineering (2016-2020).</p>
+        <p>My final year project was building a <a href="https://github.com/saket-shetty/Human-Posture-Recognition">Human Posture Detection</a> program that will detect human posture from images and video using ML and Deep Learning.</p>
+        <p>I like to work on mobile app technology like Flutter and Android(Java) and backend using Nodejs.</p>
         <p>Some of my projects :</p>
         <table>
           <tbody>
